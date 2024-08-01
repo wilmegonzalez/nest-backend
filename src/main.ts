40405e4b3c11f71626b97bf9b7cfe07cbae9d239
Wilme Gonzalez <wilmerosario20@gmail.com>
2024-08-01 16:53:47 -0400

    udpate en main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const port = process.env.PORT || 4000;
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     })
   )
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port);
 }
 bootstrap();
